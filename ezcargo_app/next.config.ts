d7b69c70da1552ceecbd246a228f461e4ef29697
danie033 <danie033@hotmail.com>
2025-07-25 21:11:38 -0600

    static

--- ezcargo_app/next.config.ts
+++ ezcargo_app/next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // 👈 enable static export
+  trailingSlash: true, // 👈 optional, but makes GitHub Pages happy
+  images: {
+    unoptimized: true // 👈 required if you're using next/image
+  },
+  basePath: "/EZCargo_next", // 👈 important for GitHub Pages routing
 };
 
 export default nextConfig;
